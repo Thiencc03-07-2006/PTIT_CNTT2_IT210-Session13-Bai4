adbfa06dbc53b18297d9bfb30d1f3ed84035ea5e
Thiencc03-07-2006 <cct20062250.@gmail.com>
2026-04-23 19:02:24 +0700

    fix

--- src/main/java/com/example/bai4/model/Medicine.java
+++ src/main/java/com/example/bai4/model/Medicine.java
@@ -1,26 +1,60 @@
 package com.example.bai4.model;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
 
 @Entity
-@Table(name = "medicine")
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
+@Table(name = "medicines")
 public class Medicine {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
-    @Column(name = "name", length = 100)
+
+    @Column(name = "name", nullable = false, length = 255)
     private String name;
-    @Column(name = "unit", length = 10)
+
+    @Column(name = "unit", length = 50)
     private String unit;
+
+    private Date expiryDate;
+
+
+    public Medicine() {
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public Date getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(Date expiryDate) {
+        this.expiryDate = expiryDate;
+    }
 }
--- src/main/java/com/example/bai4/repository/MedicineRepository.java
+++ src/main/java/com/example/bai4/repository/MedicineRepository.java
@@ -3,9 +3,10 @@ package com.example.bai4.repository;
 import com.example.bai4.model.Medicine;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-
+import java.util.Date;
 import java.util.List;
 
 @Repository
@@ -14,12 +15,11 @@ public class MedicineRepository {
     @Autowired
     private SessionFactory sessionFactory;
 
-    public List<Medicine> findByName(String name) {
-        try (Session session = sessionFactory.openSession()) {
-            return session.createQuery(
-                            "FROM Medicine m WHERE m.name = :name", Medicine.class)
-                    .setParameter("name", name)
-                    .list();
-        }
+    public List<Medicine> findExpiredMedicines() {
+        Session session = sessionFactory.getCurrentSession();
+        String hql = "FROM Medicine m WHERE m.expiryDate < :currentDate";
+        Query<Medicine> query = session.createQuery(hql, Medicine.class);
+        query.setParameter("currentDate", new Date());
+        return query.getResultList();
     }
 }
